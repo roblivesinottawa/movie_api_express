592395c79998b6f561a551de612786ded60db751
Robson Ferreira <tech.rob@icloud.com>
2021-06-04 14:19:22 -0400

    added routes

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -12,17 +12,74 @@ exports.create = (req, res) => {
 
   Movie.create(movie, (err, data) =>
     err
-      ? res
-          .status(500)
-          .send({
-            message: err.message || `An error occurred while creating movie`,
-          })
+      ? res.status(500).send({
+          message: err.message || `An error occurred while creating movie`,
+        })
       : res.send(data),
   );
 };
 
-exports.findAll = (req, res) => {};
-exports.findOne = (req, res) => {};
-exports.update = (req, res) => {};
-exports.delete = (req, res) => {};
-exports.deleteAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  Movie.getAll((err, data) => {
+    err
+      ? res.status(500).send({
+          message: err.message || `An error occurred while retrieving movies`,
+        })
+      : res.send(data);
+  });
+};
+exports.findOne = (req, res) => {
+  Movie.findById(req.params.movieId, (err, data) => {
+    if (err) {
+      err.kind == "not found"
+        ? res
+            .status(404)
+            .send({ message: `Not found movie with id ${req.params.movieId}.` })
+        : res.status(500).send({
+            message: `Error retrieving movie with id ${req.params.movieId}`,
+          });
+    } else res.send(data);
+  });
+};
+exports.update = (req, res) => {
+  if (!req.body) res.status(400).send({ message: `Content cannot be empty!` });
+
+  Movie.updateById(req.params.movieId, new Movie(req.body), (err, data) => {
+    if (err) {
+      err.kind == "not found"
+        ? res
+            .status(404)
+            .send({ message: `Not found movie with id ${req.params.movieId}.` })
+        : res.status(500).send({
+            message: `Error retrieving movie with id ${req.params.movieId}`,
+          });
+    } else res.send(data);
+  });
+};
+exports.delete = (req, res) => {
+  Movie.remove(req.params.movieId, (err, data) => {
+    if (err) {
+      err.kind == "not found"
+        ? res
+            .status(404)
+            .send({ message: `Not found movie with id ${req.params.movieId}.` })
+        : res.status(500).send({
+            message: `Could not delete movie with id ${req.params.movieId}`,
+          });
+    } else res.send({ message: `Movie was successfully deleted` });
+  });
+};
+exports.deleteAll = (req, res) => {
+  Movie.removeAll((err, data) => {
+    err
+      ? res
+          .status(500)
+          .send({
+            message:
+              err.message || `An error occurred when removing all movies.`,
+          })
+      : res.send({
+          message: `All movies were deleted successfully.`,
+        });
+  });
+};
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.json({ message: "Welcome to the movie app." }));
 
-require("./routes/movie.routes");
-const PORT = process.env.PORT || 5000;
+require("./routes/movie.routes")(app);
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`app running at localhost:${PORT}`));
